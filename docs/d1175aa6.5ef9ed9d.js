b72f1808b0975e4ccef83a5f423ef179446ea256
Kevin Viglucci <kviglucci@gmail.com>
2021-01-25 21:54:26 -0600

    update build output

---
+++ docs/d1175aa6.5ef9ed9d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{82:function(e){e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/2018/10/08/"}]}')}}]);
