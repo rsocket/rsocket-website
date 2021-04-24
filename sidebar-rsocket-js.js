1ee97aa912bc415245867811a6df74f5016c396c
Kevin Viglucci <kviglucci@gmail.com>
2021-04-24 00:25:27 -0500

    docs: expand on rsocket-js client and server docs

--- sidebar-rsocket-js.js
+++ sidebar-rsocket-js.js
@@ -2,19 +2,21 @@ module.exports = [
   "docs/guides/rsocket-js/index",
   {
     "Client": [
-      "docs/guides/rsocket-js/client/index",
+      "docs/guides/rsocket-js/client/introduction",
       "docs/guides/rsocket-js/client/rsocket-tcp-client",
       "docs/guides/rsocket-js/client/rsocket-websocket-client",
     ]
   },
   {
     "Server": [
-      "docs/guides/rsocket-js/server/index",
+      "docs/guides/rsocket-js/server/introduction",
+      "docs/guides/rsocket-js/server/rsocket-tcp-server",
+      "docs/guides/rsocket-js/server/rsocket-websocket-server",
     ]
   },
   {
     "rsocket-flowable": [
-      "docs/guides/rsocket-js/rsocket-flowable/index",
+      "docs/guides/rsocket-js/rsocket-flowable/introduction",
       "docs/guides/rsocket-js/rsocket-flowable/flowable",
       "docs/guides/rsocket-js/rsocket-flowable/single",
     ]
