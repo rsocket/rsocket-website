78384a6951e0eef65d79050cb6a5258f9d2b2f8b
gabi <gabis@precog.co>
2022-01-30 18:38:39 +0200

    initial python guide based on javascript guide

---
+++ sidebar-rsocket-py.js
@@ -0,0 +1,24 @@
+module.exports = [
+  "guides/rsocket-py/index",
+  {
+    "Client": [
+      "guides/rsocket-py/client/introduction",
+      "guides/rsocket-py/client/rsocket-tcp-client",
+      "guides/rsocket-py/client/rsocket-websocket-client",
+    ]
+  },
+  {
+    "Server": [
+      "guides/rsocket-py/server/introduction",
+      "guides/rsocket-py/server/rsocket-tcp-server",
+      "guides/rsocket-py/server/rsocket-websocket-server",
+    ]
+  },
+  {
+    "rsocket-flowable": [
+      "guides/rsocket-py/rsocket-rxpy/introduction",
+      "guides/rsocket-py/rsocket-rxpy/flowable",
+      "guides/rsocket-py/rsocket-rxpy/single",
+    ]
+  }
+];
