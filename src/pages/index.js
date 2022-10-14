620d21ff6bdcd1f08ca96be2a302b2d3ac2a2ccb
gabis@precog.co <gabis@precog.co>
2022-10-14 10:34:15 +0300

    added python homepage example - code formatting fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -227,33 +227,32 @@ from rsocket.transports.tcp import TransportTCP
 
 
 class Handler(BaseRequestHandler):
-async def request_response(self, payload: Payload) -> Awaitable[Payload]:
-logging.info(payload.data)
+    async def request_response(self, payload: Payload) -> Awaitable[Payload]:
+        logging.info(payload.data)
 
-return create_future(Payload(b'Echo: ' + payload.data))
+        return create_future(Payload(b'Echo: ' + payload.data))
 
 
 async def run_server():
-def session(*connection):
-RSocketServer(TransportTCP(*connection), handler_factory=Handler)
+    def session(*connection):
+        RSocketServer(TransportTCP(*connection), handler_factory=Handler)
 
-server = await asyncio.start_server(session, 'localhost', 7878)
+    server = await asyncio.start_server(session, 'localhost', 7878)
 
-async with server:
-await server.serve_forever()
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == '__main__':
-logging.basicConfig(level=logging.INFO)
-asyncio.run(run_server())
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(run_server())
 `}
             </CodeBlock>
           </div>
           <div>
             <h3>Client Example</h3>
             <CodeBlock className="language-python">
-              {`
-              import asyncio
+              {`import asyncio
 import logging
 
 from rsocket.helpers import single_transport_provider
