b0a46cd8e633d3fe66335f8bd7376556e2c0ae6b
gabis@precog.co <gabis@precog.co>
2022-10-14 10:29:51 +0300

    added python homepage example

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,7 @@ function Examples() {
     { label: "Kotlin", value: "kotlin" },
     { label: "C++", value: "cpp" },
     { label: "TypeScript", value: "ts" },
+    { label: "Python", value: "python"}
   ];
   return (
     <>
@@ -207,6 +208,78 @@ rsocket.requestResponse(
     onExtension: () => { },
   }
 );`}
+            </CodeBlock>
+          </div>
+        </TabItem>
+        <TabItem value={"python"}>
+          <div style={{ marginBottom: 10 }}>
+            <h3>Server Example</h3>
+            <CodeBlock className="language-python">
+              {`import asyncio
+import logging
+
+from rsocket.helpers import create_future
+from rsocket.local_typing import Awaitable
+from rsocket.payload import Payload
+from rsocket.request_handler import BaseRequestHandler
+from rsocket.rsocket_server import RSocketServer
+from rsocket.transports.tcp import TransportTCP
+
+
+class Handler(BaseRequestHandler):
+async def request_response(self, payload: Payload) -> Awaitable[Payload]:
+logging.info(payload.data)
+
+return create_future(Payload(b'Echo: ' + payload.data))
+
+
+async def run_server():
+def session(*connection):
+RSocketServer(TransportTCP(*connection), handler_factory=Handler)
+
+server = await asyncio.start_server(session, 'localhost', 7878)
+
+async with server:
+await server.serve_forever()
+
+
+if __name__ == '__main__':
+logging.basicConfig(level=logging.INFO)
+asyncio.run(run_server())
+`}
+            </CodeBlock>
+          </div>
+          <div>
+            <h3>Client Example</h3>
+            <CodeBlock className="language-python">
+              {`
+              import asyncio
+import logging
+
+from rsocket.helpers import single_transport_provider
+from rsocket.payload import Payload
+from rsocket.rsocket_client import RSocketClient
+from rsocket.rx_support.rx_rsocket import RxRSocket
+from rsocket.transports.tcp import TransportTCP
+
+
+async def main():
+    connection = await asyncio.open_connection('localhost', 7878)
+
+    async with RSocketClient(single_transport_provider(TransportTCP(*connection))) as client:
+
+        rx_client = RxRSocket(client)
+        payload = Payload(b'Hello World')
+
+        result = await rx_client.request_response(payload).pipe()
+
+        logging.info(result.data)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
+`}
             </CodeBlock>
           </div>
         </TabItem>
