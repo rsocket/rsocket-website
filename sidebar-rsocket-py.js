6581df5986f9e270a14416b74458e2c99a8d83a6
gabis@precog.co <gabis@precog.co>
2022-10-24 11:29:37 +0300

    python initial tutorial - sidebar updates

--- sidebar-rsocket-py.js
+++ sidebar-rsocket-py.js
@@ -1,6 +1,12 @@
 module.exports = [
   "guides/rsocket-py/index",
   {
+    "Guides": [
+      "guides/rsocket-py/simple",
+      "guides/rsocket-py/tutorial"
+    ]
+  },
+  {
     "Client": [
       "guides/rsocket-py/client/introduction",
       "guides/rsocket-py/client/rsocket-tcp-client",
