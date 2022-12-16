c2424350e3242fffc325768166e2d9b234ce3455
jell-o-fishi <jellofishi@pm.me>
2022-12-16 14:32:13 +0200

    rsocket-java - initial guide structure copied from python

---
+++ sidebar-rsocket-java.js
@@ -0,0 +1,13 @@
+module.exports = [
+  "guides/rsocket-java/index",
+  {
+    "Tutorial":
+      [
+        "guides/rsocket-java/tutorial/index",
+        "guides/rsocket-java/tutorial/base",
+        "guides/rsocket-java/tutorial/request_routing",
+        "guides/rsocket-java/tutorial/user_session",
+        "guides/rsocket-java/tutorial/messages"
+      ]
+  }
+];
