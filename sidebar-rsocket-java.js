c558479e45aa52542a88d1752003b8edf6735be0
jell-o-fishi <jellofishi@pm.me>
2022-12-18 20:02:26 +0200

    rsocket-java - added more java sections

--- sidebar-rsocket-java.js
+++ sidebar-rsocket-java.js
@@ -7,7 +7,10 @@ module.exports = [
         "guides/rsocket-java/tutorial/base",
         "guides/rsocket-java/tutorial/request_routing",
         "guides/rsocket-java/tutorial/user_session",
-        "guides/rsocket-java/tutorial/messages"
+        "guides/rsocket-java/tutorial/messages",
+        "guides/rsocket-java/tutorial/channels",
+        "guides/rsocket-java/tutorial/files",
+        "guides/rsocket-java/tutorial/statistics"
       ]
   }
 ];
